b04b17dd21bd21c9663739039effa446512720a9
Mankena <vmankena@BANL13013fa7c.local>
2016-07-01 11:47:57 +0530

    sum of sums inner functions

---
+++ closures/sumOfSums.js
@@ -0,0 +1,12 @@
+function sum(a){
+	var res=a;
+	function innerSum(b){
+		res+=b;
+		return innerSum;
+	}
+	innerSum.toString=function(){return res}
+	return innerSum;
+}
+
+console.log(sum(2)(3)(4).toString());
+console.log(sum(2)(3)(4)(5)(6).toString());
