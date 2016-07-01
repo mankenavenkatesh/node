e284dff66e27d2c7b3b67d6b436b564989a12de4
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2016-07-01 14:21:15 +0530

    learning decorators

---
+++ decorators/checkPermissionDecorator.js
@@ -0,0 +1,4 @@
+/*
+	Decorator function will take function as a parameter, wraps the function call, returns the wrapper, which alerts default behaviour.
+**/
+
---
+++ decorators/doublingDecorator.js
@@ -0,0 +1,12 @@
+function doublingDecorator(f){
+	return function(){
+		return 2*f.apply(this,arguments);
+	}
+}
+
+function sum(a,b){
+	return a+b;
+}
+console.log(sum(2,3));
+sum=doublingDecorator(sum);
+console.log(sum(2,3));
