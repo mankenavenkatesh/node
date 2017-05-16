2ff1d43b997c4c0a1dcf028e2e2e0a33c766a43b
Mankena <vmankena@BANL15013fa7c.local>
2017-05-16 17:16:53 +0530

    CRUD application using express and mongodb

---
+++ mongodatabase/CRUDApplication/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser=require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs')
+
+// urlencoded method will tell bodyparser to extract data from form element and add them to body property in the request object.
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req, res)=>{
+    db.collection('quotes').find().toArray((err, results)=>{
+        if(err) return console.log(err)
+        console.log(results)
+        res.render('index.ejs', {quotes:results})    
+    })    
+})
+
+app.post("/quotes", (req, res)=>{
+    console.log(req.body)
+    db.collection("quotes").save(req.body, (err, result)=>{
+        if(err) return console.log(err)
+        console.log("saved to db")
+         res.redirect('/allquotes')
+    })
+})
+
+var db
+var linkToMongoDb = 'mongodb://localhost:27017/test'
+MongoClient.connect(linkToMongoDb, (err, database)=>{
+   if(err) return console.log(err)
+    db=database
+    app.listen(3000, function(){
+        console.log('listenting on 3000')
+    })
+});
